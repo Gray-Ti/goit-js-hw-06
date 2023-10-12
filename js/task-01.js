08b9ee998653603f475c53a3f8078b761c95a7e0
ShanTi <karbid4mo@gmail.com>
2023-10-12 15:57:36 +0300

    add new

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,14 @@
 const categoriesList = document.querySelectorAll('.item');
 console.log(`Number of categoris: ${categoriesList.length}`);
 
+const firstCategotiesItem = item.firstElementChild;
+
+const lastCategoriesItem = item.lastElementChild;
+
+const listItem = item.children;
+
+
+
 categoriesList.forEach(category => {
     const categoryName = category.querySelector('h2').textContent;
     const elementsCount = category.querySelectorAll('ul > li').length;
